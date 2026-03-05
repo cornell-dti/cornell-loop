94a3c32933cd1fd7265af877be9cefb2f283ad9e
Annie Chen <anc76@cornell.edu>
2026-03-05 14:03:58 -0500

    remove auth testing in app.tsx
    - replace for now with placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useConvexAuth } from 'convex/react';
-import { SignIn, SignOut } from './Auth';
 import './App.css';
 
 function App() {
-  const { isAuthenticated, isLoading } = useConvexAuth();
-
-  if (isLoading){
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-  }
+  
   return (
-    <>
-      {isAuthenticated ? <SignOut /> : <SignIn />}
-    </>
+    <div>
+      App
+    </div>
   )
 };
 
