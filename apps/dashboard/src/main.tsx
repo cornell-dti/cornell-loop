b78c9dc8032d5cdc6f4a1b7e2f7519b43801cada
Annie Chen <anc76@cornell.edu>
2026-09-23 20:04:02 -0400

    install posthog

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -4,16 +4,25 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { ConvexReactClient } from "convex/react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import App from "./App.tsx";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  defaults: "2026-05-30",
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConvexAuthProvider client={convex}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ConvexAuthProvider>
+    <PostHogProvider client={posthog}>
+      <ConvexAuthProvider client={convex}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConvexAuthProvider>
+    </PostHogProvider>
   </StrictMode>,
 );
